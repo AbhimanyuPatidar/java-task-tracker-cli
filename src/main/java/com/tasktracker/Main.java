b175179ad1031d94f470efbfa024d94e52332f4f
AbhimanyuPatidar <abhimanyupatidar2511@gmail.com>
2026-04-21 18:27:04 +0530

    feat: initialize project structure

---
+++ src/main/java/com/tasktracker/Main.java
@@ -0,0 +1,14 @@
+package com.tasktracker;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Usage: task-cli <command>");
+            return;
+        }
+
+        // TaskManager manager = new TaskManager();
+
+        // Switch-case to be added
+    }
+}
---
+++ src/main/java/com/tasktracker/TaskManager.java
@@ -0,0 +1,5 @@
+package com.tasktracker;
+
+public class TaskManager {
+    // Logic layer
+}
